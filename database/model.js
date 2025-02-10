17d984b45789beb61bba43eeffe66b2d26a2ce58
wanzofc <berlianawan498@gmail.com>
2025-02-10 08:12:13 +0700

    Update model.js

--- database/model.js
+++ database/model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
 const Users = mongoose.Schema({
     username: { type: String },
-    password: { type: String},
+    password: { type: String },
     apikey: { type: String },
     defaultKey: { type: String },
     premium: { type: Array },
-    limit: { type: Number }
+    limit: { type: Number },
+    status: { type: String, default: 'active' } // Tambahkan kolom status
 }, { versionKey: false });
 module.exports.User = mongoose.model('api', Users);
